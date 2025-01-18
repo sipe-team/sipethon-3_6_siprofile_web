004235250fa31a216c9d516cca21f4aa018425cb
chanhee.dev <chanhee.dev@gmail.com>
2025-01-18 19:32:50 +0900

    fix: 페이지 설명 문구 수정건 반영

--- siprofile_web/src/pages/CreatePage.tsx
+++ siprofile_web/src/pages/CreatePage.tsx
@@ -1,4 +1,4 @@
-import { useState, ReactElement, useContext } from 'react';
+import { useState, ReactElement, useContext, ChangeEvent } from 'react';
 import PageContainer from '../components/PageContainer';
 import PageDescription from '../components/PageDescription';
 import { TextField, Button } from '@mui/material';
@@ -9,43 +9,65 @@ import CreateCardContext from '../context/CreateCardContext';
 import PageHistoryContext from '../context/PageHistoryContext';
 
 const Color = () => {
-  return (
-    <>
-      <PageDescription pageTitle="배경 컬러" pageSubTitle="자유롭게 고민중" />
-      <FloatingButtonContainer>
-        <ContainedPrimaryButton
-          buttonProps={{
-            onClick: () => {
-              console.log('SUBMIT');
-            },
-          }}
-        >
-          다음
-        </ContainedPrimaryButton>
-      </FloatingButtonContainer>
-    </>
-  );
-};
+  const createCardContext = useContext(CreateCardContext);
 
-const Link = () => {
-  const pageHistoryContext = useContext(PageHistoryContext);
-  if (pageHistoryContext) {
-    const { pageHistory, setPageHistory } = pageHistoryContext;
+  if (createCardContext) {
     return (
       <>
-        <PageDescription pageTitle="링크" pageSubTitle="보여주고 싶은" />
-        <TextField placeholder="링크" variant="standard" />
+        <PageDescription pageTitle="배경 컬러" pageSubTitle="프로필 카드의 배경으로 사용할" />
         <FloatingButtonContainer>
           <ContainedPrimaryButton
             buttonProps={{
               onClick: () => {
-                setPageHistory([...pageHistory, <Color />]);
+                console.log('SUBMIT');
               },
             }}
           >
             다음
           </ContainedPrimaryButton>
-          <Button variant="outlined">SKIP</Button>
+        </FloatingButtonContainer>
+      </>
+    );
+  }
+  return null;
+};
+
+const Link = () => {
+  const pageHistoryContext = useContext(PageHistoryContext);
+  const createCardContext = useContext(CreateCardContext);
+
+  if (pageHistoryContext && createCardContext) {
+    const { pageHistory, setPageHistory } = pageHistoryContext;
+    const { link, setLink } = createCardContext;
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+      const { currentTarget } = event;
+      setLink(currentTarget.value);
+    };
+
+    const handleNext = () => {
+      setPageHistory([...pageHistory, <Color />]);
+    };
+
+    return (
+      <>
+        <PageDescription
+          pageTitle="링크가 있다면 입력해 주세요"
+          pageSubTitle="추가로 보여주고 싶은"
+        />
+        <TextField placeholder="링크" variant="standard" value={link} onChange={handleChange} />
+        <FloatingButtonContainer>
+          <ContainedPrimaryButton
+            buttonProps={{
+              disabled: link.length === 0,
+              onClick: handleNext,
+            }}
+          >
+            다음
+          </ContainedPrimaryButton>
+          <Button variant="outlined" onClick={handleNext}>
+            SKIP
+          </Button>
         </FloatingButtonContainer>
       </>
     );
@@ -55,12 +77,26 @@ const Link = () => {
 
 const Keyword = () => {
   const pageHistoryContext = useContext(PageHistoryContext);
-  if (pageHistoryContext) {
+  const createCardContext = useContext(CreateCardContext);
+
+  if (pageHistoryContext && createCardContext) {
     const { pageHistory, setPageHistory } = pageHistoryContext;
+    const { setKeyword } = createCardContext;
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { currentTarget } = event;
+      setKeyword(
+        currentTarget.value
+          .split(',')
+          .map((item) => item.trim())
+          .filter((item) => item.length !== 0),
+      );
+    };
+
     return (
       <>
         <PageDescription pageTitle="키워드를 작성해 주세요" pageSubTitle="자신을 표현할 수 있는" />
-        <TextField placeholder="키워드" variant="standard" />
+        <TextField placeholder="키워드" variant="standard" onChange={handleChange} />
         <FloatingButtonContainer>
           <ContainedPrimaryButton
             buttonProps={{
@@ -80,11 +116,15 @@ const Keyword = () => {
 
 const Position = () => {
   const pageHistoryContext = useContext(PageHistoryContext);
-  if (pageHistoryContext) {
+  const createCardContext = useContext(CreateCardContext);
+
+  if (pageHistoryContext && createCardContext) {
     const { pageHistory, setPageHistory } = pageHistoryContext;
+    const { name, position, setPosition } = createCardContext;
+
     return (
       <>
-        <PageDescription pageTitle="포지션 선택" pageSubTitle="포지션 선택" />
+        <PageDescription pageTitle="포지션을 선택해 주세요" pageSubTitle={`${name} 님의`} />
         <TextField placeholder="이름" variant="standard" />
         <FloatingButtonContainer>
           <ContainedPrimaryButton
@@ -105,15 +145,25 @@ const Position = () => {
 
 const Name = () => {
   const pageHistoryContext = useContext(PageHistoryContext);
-  if (pageHistoryContext) {
+  const createCardContext = useContext(CreateCardContext);
+
+  if (pageHistoryContext && createCardContext) {
     const { pageHistory, setPageHistory } = pageHistoryContext;
+    const { name, setName } = createCardContext;
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { currentTarget } = event;
+      setName(currentTarget.value);
+    };
+
     return (
       <>
-        <PageDescription pageTitle="이름과 이미지" pageSubTitle="이름과 이미지" />
-        <TextField placeholder="이름" variant="standard" />
+        <PageDescription pageTitle="이름을 입력해 주세요" pageSubTitle="사이프로필에서 사용할" />
+        <TextField placeholder="이름" variant="standard" value={name} onChange={handleChange} />
         <FloatingButtonContainer>
           <ContainedPrimaryButton
             buttonProps={{
+              disabled: name.length === 0,
               onClick: () => {
                 setPageHistory([...pageHistory, <Position />]);
               },
